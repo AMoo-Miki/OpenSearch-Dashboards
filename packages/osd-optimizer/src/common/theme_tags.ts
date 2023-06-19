86695953b8bb106405a66d0faeed858151c47d2a
Miki <miki@amazon.com>
2023-06-19 01:09:20 -0700

    Serve `color-scheme`-aware stylesheets
    Signed-off-by: Miki <miki@amazon.com>

--- packages/osd-optimizer/src/common/theme_tags.ts
+++ packages/osd-optimizer/src/common/theme_tags.ts
@@ -38,9 +38,9 @@ const isArrayOfStrings = (input: unknown): input is string[] =>
   Array.isArray(input) && input.every((v) => typeof v === 'string');
 
 export type ThemeTags = readonly ThemeTag[];
-export type ThemeTag = 'v7light' | 'v7dark' | 'v8light' | 'v8dark';
-export const DEFAULT_THEMES = tags('v7light', 'v7dark');
-export const ALL_THEMES = tags('v7light', 'v7dark', 'v8light', 'v8dark');
+export type ThemeTag = 'v7light' | 'v7dark' | 'v8light' | 'v8dark' | 'v7' | 'v8';
+export const DEFAULT_THEMES = tags('v7light', 'v7dark', 'v8light', 'v8dark', 'v7', 'v8');
+export const ALL_THEMES = tags('v7light', 'v7dark', 'v8light', 'v8dark', 'v7', 'v8');
 
 export function parseThemeTags(input?: any): ThemeTags {
   if (!input) {
--- src/legacy/ui/ui_render/ui_render_mixin.js
+++ src/legacy/ui/ui_render/ui_render_mixin.js
@@ -109,21 +109,11 @@ export function uiRenderMixin(osdServer, server, config) {
 
       const styleSheetPaths = [
         `${regularBundlePath}/osd-ui-shared-deps/${UiSharedDeps.baseCssDistFilename}`,
-        ...(darkMode
-          ? [
-              themeVersion === 'v7'
-                ? `${regularBundlePath}/osd-ui-shared-deps/${UiSharedDeps.darkCssDistFilename}`
-                : `${regularBundlePath}/osd-ui-shared-deps/${UiSharedDeps.darkV8CssDistFilename}`,
-              `${basePath}/node_modules/@osd/ui-framework/dist/kui_dark.css`,
-              `${basePath}/ui/legacy_dark_theme.css`,
-            ]
-          : [
-              themeVersion === 'v7'
-                ? `${regularBundlePath}/osd-ui-shared-deps/${UiSharedDeps.lightCssDistFilename}`
-                : `${regularBundlePath}/osd-ui-shared-deps/${UiSharedDeps.lightV8CssDistFilename}`,
-              `${basePath}/node_modules/@osd/ui-framework/dist/kui_light.css`,
-              `${basePath}/ui/legacy_light_theme.css`,
-            ]),
+        themeVersion === 'v7'
+          ? `${regularBundlePath}/osd-ui-shared-deps/${UiSharedDeps.cssDistFilename}`
+          : `${regularBundlePath}/osd-ui-shared-deps/${UiSharedDeps.cssV8CssDistFilename}`,
+        `${basePath}/node_modules/@osd/ui-framework/dist/kui.css`,
+        `${basePath}/ui/legacy_theme.css`,
       ];
 
       const kpUiPlugins = osdServer.newPlatform.__internals.uiPlugins;
