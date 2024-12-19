cd6dc3a5f0f5361f6aea4b70b24edffe9afb2990
Miki <miki@amazon.com>
2024-12-18 16:17:37 -0800

    Feature detection
    Signed-off-by: Miki <miki@amazon.com>

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,8 +3,12 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+/* eslint-disable no-console */
+
+import { setTimeout } from 'timers/promises';
 import { defineConfig } from 'cypress';
 import webpackPreprocessor from '@cypress/webpack-preprocessor';
+import fetch from 'node-fetch';
 
 module.exports = defineConfig({
   defaultCommandTimeout: 60000,
@@ -22,7 +26,6 @@ module.exports = defineConfig({
       url: 'http://localhost:9200',
     },
     openSearchUrl: 'http://localhost:9200',
-    SECURITY_ENABLED: false,
     AGGREGATION_VIEW: false,
     username: 'admin',
     password: 'myStrongPassword123!',
@@ -32,6 +35,9 @@ module.exports = defineConfig({
     DATASOURCE_MANAGEMENT_ENABLED: false,
     ML_COMMONS_DASHBOARDS_ENABLED: true,
     WAIT_FOR_LOADER_BUFFER_MS: 0,
+
+    // This value is automatically determined at runtime
+    SECURITY_ENABLED: false,
     codeCoverage: {
       url: 'http://localhost:5601/__coverage__',
     },
@@ -39,7 +45,6 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5601',
     specPattern: 'cypress/integration/**/*.spec.{js,jsx,ts,tsx}',
-    testIsolation: false,
     setupNodeEvents,
   },
 });
@@ -74,6 +79,29 @@ function setupNodeEvents(
       webpackOptions,
     })
   );
+
+  on('before:run', async (details) => {
+    const startTime = Date.now();
+    do {
+      const resp = await fetch('https://miki.osd.aws.barahmand.com/api/status');
+
+      if (resp.status === 200) {
+        console.log('OpenSearch Dashboards is configured without security');
+        Cypress.env('SECURITY_ENABLED', false);
+        Cypress.config('testIsolation', true);
+        break;
+      }
+
+      if (resp.status === 401) {
+        console.log('OpenSearch Dashboards is configured with security');
+        Cypress.env('SECURITY_ENABLED', true);
+        break;
+      }
+
+      console.log('Waiting for OpenSearch Dashboards to be ready...');
+      await setTimeout(15000);
+    } while (Date.now() - startTime < 60000);
+  });
 
   return config;
 }
